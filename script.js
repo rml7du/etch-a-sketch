dc9e463ca6cbc28f29ad9fc2d2427fd981c43f0f
Robert Lee <rml7du@gmail.com>
2020-05-18 11:28:25 -0400

    updated CSS and JS to paint the grid

--- script.js
+++ script.js
@@ -3,14 +3,19 @@
 let board = document.querySelector(".container");
 
 
-function createBoxes() {
-    for (i=0; i<256; i++){
+function createBoxes(gridSize) {
+    for (i=0; i<gridSize; i++){
         var box = document.createElement('div');
         box.className = 'etchSketch';
         //document.getElementById('boxes').appendChild(boxes);
-        console.log(i);
+        //console.log(i);
         board.appendChild(box);
     }
 }
 
-createBoxes();
+board.addEventListener("mouseover", (e) => { 
+    //e.target.setAttribute("class", "etchSketchBlack");
+    e.target.style.backgroundColor = "red";
+  } )
+
+createBoxes(256);

83bb0eb1f759bfcf56a0b2185a2215accec11a91
Robert Lee <rml7du@gmail.com>
2020-05-18 10:01:24 -0400

    js and css for creating 256 boxes is operational

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 
-let board = document.querySelector("#container");
+let board = document.querySelector(".container");
 
 
 function createBoxes() {
     for (i=0; i<256; i++){
-        var boxes = document.createElement('div');
-        boxes.className = 'etchSketch';
-        document.getElementById('boxes').appendChild(boxes);
-        //board.appendChild(boxes);
+        var box = document.createElement('div');
+        box.className = 'etchSketch';
+        //document.getElementById('boxes').appendChild(boxes);
+        console.log(i);
+        board.appendChild(box);
     }
 }
+
+createBoxes();

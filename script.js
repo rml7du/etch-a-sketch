4eb927cb7787d4fc61d7a80da7a93290d8fff664
Robert Lee <rml7du@gmail.com>
2020-05-20 12:36:22 -0400

    fully functional. updated js to include add event listener to each created div (so gradient would work), and updated the style document.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const resetValue = 16;
-const resetButton = document.getElementById("resetButton");
+
 let UserColorValue;
-
-
-
-
 let board = document.querySelector('.container');
 
 
@@ -19,48 +14,96 @@ function createBoxes(gridSize) {
 
 //----------Black  Button--------------//
 const blackButton = document.getElementById('black');
-blackButton.addEventListener('click', black);
+blackButton.addEventListener('click', goBlack);
+
+function goBlack() {
+    const gridElements = document.querySelectorAll('.etchSketch');
+        gridElements.forEach( (div) => {div.removeEventListener('mouseenter', gradient); });
+        gridElements.forEach( (div) => {div.removeEventListener('mouseenter', random); });
+        gridElements.forEach( (div) => {div.removeEventListener('mouseenter', userColor); });
+        gridElements.forEach( (div) => {div.addEventListener('mouseenter', black); });
+}
 
 function black() {
-    board.addEventListener('mouseover', (e) => { 
-        e.target.style.backgroundColor = 'black'
-    });
+
+        this.style.backgroundColor = 'black';
+    
 }
 
 //----------Gradient Color Button--------------//
+
 const gradientButton = document.getElementById('gradientColor');
-gradientButton.addEventListener('click', gradient);
+gradientButton.addEventListener('click', goGradient);
+
+function goGradient(){
+
+        const gridElements = document.querySelectorAll('.etchSketch');
+        gridElements.forEach( (div) => {div.removeEventListener('mouseenter', black); });
+        gridElements.forEach( (div) => {div.removeEventListener('mouseenter', random); });
+        gridElements.forEach( (div) => {div.removeEventListener('mouseenter', userColor); });
+        gridElements.forEach( (div) => {div.addEventListener('mouseenter', gradient); });
+}
 
 function gradient() {
-    board.addEventListener('mouseover', (e) => { 
-        e.target.style.backgroundColor = 'black'
-    });
+    
+    const style = window.getComputedStyle(this);
+    const backgroundColorValue = style.getPropertyValue('background-color'); 
+
+    const firstComma = backgroundColorValue.indexOf(',');
+    const firstSpace = backgroundColorValue.indexOf(' ');
+    const lastComma = backgroundColorValue.lastIndexOf(',');
+    const lastSpace = backgroundColorValue.lastIndexOf(' ');
+
+    const valueColorR = backgroundColorValue.slice('4', firstComma);
+    const valueColorG = backgroundColorValue.slice((firstSpace+1), lastComma);
+    const valueColorB = backgroundColorValue.slice((lastSpace+1),-1);
+
+    const newValueColorR = valueColorR - (valueColorR*0.1);
+    const newValueColorG = valueColorG - (valueColorG*0.1);
+    const newValueColorB = valueColorB - (valueColorB*0.1);
+
+ 
+    this.style.backgroundColor = `rgb(${newValueColorR},${newValueColorG},${newValueColorB})`;
+    
 }
 
 
 //----------Random Color Button--------------//
 const randomButton = document.getElementById('randomColor');
-randomButton.addEventListener('click', random);
+randomButton.addEventListener('click', goRandom);
+
+function goRandom(){
+    const gridElements = document.querySelectorAll('.etchSketch');
+    gridElements.forEach( (div) => {div.removeEventListener('mouseenter', black); });
+    gridElements.forEach( (div) => {div.removeEventListener('mouseenter', gradient); });
+    gridElements.forEach( (div) => {div.removeEventListener('mouseenter', userColor); });
+    gridElements.forEach( (div) => {div.addEventListener('mouseenter', random); });
+}
 
 function random() {
-    board.addEventListener('mouseover', (e) => { 
-        e.target.style.backgroundColor = 'rgb('+ Math.floor(Math.random() * 256) +','
+        this.style.backgroundColor = 'rgb('+ Math.floor(Math.random() * 256) +','
         + Math.floor(Math.random() * 256) +','
         + Math.floor(Math.random() * 256) +')';
-    });
+    
 }
 
 ///----------User Color Button--------------//
 const userColorButton = document.getElementById('userColor');
-userColorButton.addEventListener('click', userColor);
-userColorButton.addEventListener('change', userColor);
+userColorButton.addEventListener('click', goUserColor);
+userColorButton.addEventListener('change', goUserColor);
 
+function goUserColor(){
+    const gridElements = document.querySelectorAll('.etchSketch');
+    gridElements.forEach( (div) => {div.removeEventListener('mouseenter', black); });
+    gridElements.forEach( (div) => {div.removeEventListener('mouseenter', gradient); });
+    gridElements.forEach( (div) => {div.removeEventListener('mouseenter', random); });
+    gridElements.forEach( (div) => {div.addEventListener('mouseenter', userColor); });
+
+    userColorValue = event.target.value;
+}
 
 function userColor() {
-    userColorValue = event.target.value;
-    board.addEventListener('mouseover', (e) => { 
-        e.target.style.backgroundColor = userColorValue;
-    });
+    this.style.backgroundColor = userColorValue;
 }
 
  
@@ -78,6 +121,7 @@ function resizeGrid() {
     board.innerHTML = '';
       if (gridSize.value > 64) {
         alert("max size is 64")
+        createBoxes(resetValue);
     } else {
         createBoxes(Math.floor(gridSize.value)) //integers only
     }
@@ -85,8 +129,9 @@ function resizeGrid() {
 
 
 
-  //Reset Grid
-
+  //----------------Reset Grid----------------//
+  const resetValue = 16;
+  const resetButton = document.getElementById("resetButton");
   resetButton.addEventListener('click', resetGrid);
 
   function resetGrid(){
@@ -94,5 +139,7 @@ function resizeGrid() {
       createBoxes(resetValue);
   }
 
+
+//---------------initialize----------------//
 createBoxes(resetValue);
-black();
+goBlack();
